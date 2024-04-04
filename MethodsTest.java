5df9351d2a0e6a4544255de2ec36dc0f63a51d3a
Viiiiikaaaaa <116728003+Viiiiikaaaaa@users.noreply.github.com>
2024-04-04 12:55:03 +0300

    MethodsTest.java

--- MethodsTest.java
+++ MethodsTest.java
@@ -1,21 +1,21 @@
 package org.example;
+import org.junit.Before;
 import org.junit.Test;
 import org.testng.Assert;
 
-
 public class MethodsTest {
-    private Methods object = new Methods();
+    @Before
+    public void newObject(){
+        Methods object = new Methods();
+    }
+
     @Test
     public void testCompareStrings() {
         Assert.assertTrue(Methods.compareStrings("abc", "def"));
-
     }
 
     @Test
     public void testFactorial() {
         Assert.assertEquals(120, Methods.factorial(5));
-
     }
-
 }
-
